07a129aad8301349e9745ca66a694c626653ac5c
higi <khiglix@>
2023-06-22 19:25:40 +0300

    ex00 done

---
+++ cpp05/ex00/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include "Bureaucrat.hpp"
+
+int main(int /*argc*/, char **/*argv*/)
+{
+	try
+	{
+		Bureaucrat james("James", 24);
+
+		std::cout << "Before gradePlus(): " << james << std::endl;
+		james.gradePlus();
+		std::cout << "After gradePlus(): " << james << std::endl;
+
+		std::cout << std::endl << std::endl;
+
+		Bureaucrat sam("Sam", 1);
+
+		std::cout << "Before gradeMinus(): " << sam << std::endl;
+		sam.gradeMinus();
+	} 
+	catch(const std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+	return (0);
+}
