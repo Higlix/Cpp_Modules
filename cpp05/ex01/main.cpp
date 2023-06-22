065565bc7de6e86e677365216639fc94c2bde109
higi <khiglix@>
2023-06-22 20:14:46 +0300

    ex01 done

---
+++ cpp05/ex01/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "Bureaucrat.hpp"
+
+int main(int /*argc*/, char **/*argv*/)
+{
+	try
+	{
+		Bureaucrat	james("James", 24);
+		Form		bizDeal("Deal", 23, 24);
+		
+		james.signForm(bizDeal);
+	} 
+	catch(const std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+	return (0);
+}
